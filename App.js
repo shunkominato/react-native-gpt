0db2d5ebdf43e4e7b71a75e30d530496e985640a
shunkominato <shun19871001@gmail.com>
2023-11-26 08:49:19 +0900

    ts

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Componetns from "./componet";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to starasdfasdfsat working on your app!</Text>
+      <Componetns />
       <StatusBar style="auto" />
     </View>
   );
